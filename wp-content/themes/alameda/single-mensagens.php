78e4fe57b45d7222e45cfb5aae70cde7aad79632
William Kunitake <william.kunitake@gmail.com>
2020-02-05 18:11:34 -0300

    Mudanças na página de single das mensagens em áudio (spotify no lugar do mixcloud)

--- wp-content/themes/alameda/single-mensagens.php
+++ wp-content/themes/alameda/single-mensagens.php
@@ -7,11 +7,12 @@
 				<div class="row">
 					<div class="col s12 heading">
 						<div class="col s12 no-padding mg150 mg120m">
-							<?php $datamensagem = get_field('data_da_mensagem', false, false); $datamensagem = new DateTime($datamensagem); ?>
-							<h1 class="white-text center hide-on-med-and-down"><?php the_title(); ?><br>
-								<span class="f23">Mensagem pregada no dia <?php echo $datamensagem->format('j/m/Y'); ?>, por <?php $pregador = get_field('pregador'); echo $pregador->name; ?></span>
-							</h1>
-							<h3 class="white-text center hide-on-large-only f30m p5m mg0m"><?php the_title(); ?> - <?php echo $datamensagem->format('j/m/Y'); ?></h3>
+							<h1 class="white-text center hide-on-med-and-down"><?php the_title(); ?></h1>
+							<h3 class="white-text center hide-on-large-only f30m p5m mg0m">
+								<div class="container">
+									<?php the_title(); ?>
+								</div>
+							</h3>
 						</div>
 					</div>
 			    </div>
@@ -19,16 +20,19 @@
 		</section>
 		<div class="container">
 			<div class="row pb50">
-				<div class="col s12 center pt50">
+				<div class="col s12 center pt50 pt10m">
 					<h3 class="turquesa-alameda-text f30">Ouça abaixo a mensagem:</h3>
 				</div>
-				<div class="col s12 m8 push-m2 pt50 pb50">
-					<iframe width="100%" height="120" src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&light=1&feed=%2F<?php echo str_replace("https://www.mixcloud.com/", "", get_field('link_do_mixcloud')); ?>" frameborder="0" >
-					</iframe>
+			    <div class="col s12 m8 push-m2 pb50">
+			    	<?php $datamensagem = get_field('data_da_mensagem', false, false); $datamensagem = new DateTime($datamensagem); ?>
+			    	<p class="center mg0m">Ouça abaixo a mensagem pregada no dia <?php echo $datamensagem->format('j/m/Y'); ?> por <?php $pregador = get_field('pregador'); echo $pregador->name; ?>.</p>
+					<div class="pt10 pt30m">
+						<iframe src="<?php the_field('url-spotify'); ?>" width="100%" height="232" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+					</div>
 			    </div>
 			</div>
 		</div>
 
 	<?php endwhile; // end of the loop. ?>
 
-<?php get_footer( );
+<?php get_footer( ); ?>
