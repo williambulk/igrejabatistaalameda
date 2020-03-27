57012991f41023bf95b018e59f3820bd41892607
William Kunitake <william.kunitake@gmail.com>
2020-03-27 11:49:52 -0400

    Alteração prévia não comitada antes: mudança na página eventos por causa do Coronavírus (remoção da data e adição do motivo)

--- wp-content/themes/alameda/single-eventos_alameda.php
+++ wp-content/themes/alameda/single-eventos_alameda.php
@@ -22,18 +22,18 @@
 					<div class="col s12 l5 push-l1 center-mobile turquesa-alameda-text">
 						<h1 class="turquesa-alameda-text f30 f30m hide-on-small-only"><?php the_title(); ?></h1>
 						<?php if(get_field('multiplos_dias') == 'sim'): ?>
-							<h3 class="turquesa-alameda-text">Data:
+							<h3 class="turquesa-alameda-text">Motivo:
 								<?php the_field('periodo_evento'); ?>
 							</h3>
 						<?php else: ?>
-							<h3 class="turquesa-alameda-text">Data:
+							<h3 class="turquesa-alameda-text">Data: 
 								<?php $dataevento = get_field('data_do_evento', false, false); $dataevento = new DateTime($dataevento); ?><?php echo $dataevento->format('j/m/Y'); ?>
 							</h3>
 							<h3 class="turquesa-alameda-text">Horário: <?php the_field('horario_evento'); ?></h3>
 						<?php endif; ?>
-						<a class="btn turquesa-alameda" target="blank" href="<?php the_field('link_do_google_calendar'); ?>">
+						<!-- <a class="btn turquesa-alameda" target="blank" href="<?php the_field('link_do_google_calendar'); ?>">
 						Adicionar no Calendário
-						</a>
+						</a> -->
 					</div>
 			    </div>
 			    <div class="col s12 m10 push-m1 justify">
