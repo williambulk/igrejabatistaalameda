4f22ea6c5084d3138a38ca297282402aae72e09b
William Kunitake <william.kunitake@gmail.com>
2020-05-04 15:09:59 -0400

    Ajuste no modelo da página page.php

--- wp-content/themes/alameda/page.php
+++ wp-content/themes/alameda/page.php
@@ -5,29 +5,27 @@
 
 get_header(); ?>
 
+<style type="text/css">
+	.invisible {
+		background-color: rgba(0, 0, 0, 0.7) !important;
+	}
+</style>
+
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<div class="container">
-		<header class="titulo-pagina">
-			<!-- BREADCRUMBS -->
-			<div class="breadcrumbs" typeof="BreadcrumbList" vocab="https://schema.org/">
-			    <?php if(function_exists('bcn_display'))
-			    {
-			        bcn_display();
-			    }?>
-			</div>
-			<!-- TÍTULO -->
-			<?php the_title( '<h1 class="titulo-pagina">', '</h1>' );	?>
-		</header>
 		<!-- INÍCIO DO WHILE -->
 		<?php while ( have_posts() ) : the_post(); ?>
-			<div class="row">
-				<div class="col s12">
-					<?php the_content(); ?>
+			<div class="container">
+				<div class="row pb50 pb50m">
+					<?php $featured_img_url = get_the_post_thumbnail_url(get_the_ID(),'full'); ?>
+					<img src="<?php echo $featured_img_url; ?>);" class="responsive-img">
+					<div class="col s12 m8 push-m2 pt30m pt50 pb50">
+						<h1 class="turquesa-alameda-text center f30m f40 pb50 pb30m mg0 mg0m"><?php the_title(); ?></h1>
+						<?php the_content(); ?>
+					</div>
 				</div>
 			</div>
 		<?php endwhile;?>
 		<!-- FIM DO WHILE -->
-	</div>
 </article>
 
 <?php get_footer(); ?>
